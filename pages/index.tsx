f502535273c36f55e892506d7c1d3398257c3259
Joven Miclat <im.jovenmiclat@gmail.com>
2022-08-19 06:06:11 +0800

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.scss";
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className="text-blue-900">mr beast 3000</h1>
+    </div>
+  );
+};
+
+export default Home;
